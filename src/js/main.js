65a7b062d846af62c7fed5651a50c6e8895eba4a
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-08-02 14:41:01 +0500

    footer added

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 // @ts-check
 "use strict";
+
+window.onload = () => {
+  document.querySelector(
+    ".copyright"
+  ).textContent = `${"\u00A9"} Copyright ${new Date().getFullYear()} by Cloud-One. Designed by Jonas Schmedtmann.`;
+};

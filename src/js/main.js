a24d3e3cf667f071ec8baa8ffbce93756b2adebf
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-09-05 13:13:21 +0500

    20190905 Build

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-"use strict";
-
-window.onload = () => {
-  document.querySelector(
-    ".copyright"
-  ).textContent = `${"\u00A9"} Copyright ${new Date().getFullYear()} by Cloud-One. Designed by Jonas Schmedtmann.`;
-};
